a6dc19a0dc7fab7b4b122515f63dbf312ffb787e
sanjueranga <sanjueranga16@gmail.com>
2023-12-21 17:29:50 +0530

    Getting Started

---
+++ backend/src/app/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = process.env['PORT'] ?? 3333;
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
+  await app.listen(port);
+  console.log(`🚀 server started on http://localhost:${port}`);
+}
+bootstrap();
