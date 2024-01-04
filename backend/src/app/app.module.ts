46a6b142759cdb8229ec2a9e13794b11890895d0
sanjueranga <sanjueranga16@gmail.com>
2024-01-04 23:25:59 +0530

    feat: configured db and env

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env'] }),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
 })
 export class AppModule {}
