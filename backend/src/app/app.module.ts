d09e6ac9437795fcccb2e2f27dfe973ce5d630be
sanjueranga <sanjueranga16@gmail.com>
2024-01-06 20:42:13 +0530

    WIP

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from 'src/users/user.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { ProjectModule } from 'src/project/project.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ['.env'] }),
     MongooseModule.forRoot(process.env.DB_URL),
+    UsersModule,
+    AuthModule,
+    ProjectModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
